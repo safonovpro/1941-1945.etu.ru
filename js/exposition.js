cbe69da068283b7a38daa9d4a7cdbc5730a22e6b
Alexey Safonov <alexey@safonov.pro>
2019-04-24 16:48:34 +0300

    Fix откуда начинаем качать данные

--- js/exposition.js
+++ js/exposition.js
@@ -24,15 +24,19 @@ class Exposition {
         while (this.wrap.lastChild) {
             this.wrap.removeChild(this.wrap.lastChild);
         }
+
+        console.log(this.data);
         
         for(let h = 0; h < wrapHeight; h += veteranHeight) {
             for(let w = 0; w < wrapWidth; w += veteranWidth) {
                 const veteran = document.createElement('div');
 
                 veteran.className = 'veteran';
+                veteran.id = this.data[index].id;
                 veteran.style.cssText = `top: ${h}px; left: ${w}px;`;
                 veteran.style.cssText += `width: ${veteranWidth}px; height: ${veteranHeight}px;`;
-                veteran.style.cssText += `background-image: url("${this.data[index++].photo.url}")`;
+                veteran.style.cssText += `background-image: url("${this.data[index].photo.url}")`;
+                index++;
 
                 this.wrap.appendChild(veteran);
             }
@@ -49,7 +53,7 @@ class Exposition {
             
             data = data.concat(firstPage[1]);
 
-            for(let i = 1; i < totalPages + 1; i++) {
+            for(let i = 2; i < totalPages + 1; i++) {
                 data = data.concat((await this._getOnePage(pageSize, i))[1]);
             }
         } catch(e) {
@@ -59,7 +63,7 @@ class Exposition {
         return data;
     }
 
-    _getOnePage(pageSize = 100, currentPage = 0) {
+    _getOnePage(pageSize = 100, currentPage = 1) {
         return window.fetch(`${this.urlAPI}?page_size=${pageSize}&current_page=${currentPage}`)
         .then(response => response.json())
         .then(json => json)
